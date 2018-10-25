2ac023994b4ce320648c0925ce4642e448d11fbc
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-10-25 13:30:04 -0500

    Update contactdetails.sql

---
+++ Database System/Tables/Abrahan Ramirez/contactdetails.sql
@@ -0,0 +1,15 @@
+/**
+*Created by Abrahan Ramirez
+*Contact Details Table
+*This relation has data about the contact deatails of passengers.
+**/
+
+
+create table Contact_Details(
+	CnID INT PRIMARY KEY,
+	Email VARCHAR(16) NOT NULL,
+	Cell VARCHAR(16) NOT NULL,
+	Street VARCHAR(64),
+	State VARCHAR(20) NOT NULL,
+	CONSTRAINT fk_State FOREIGN KEY(State) REFERENCES State(StID)
+);
