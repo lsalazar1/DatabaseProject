9003c6d0b70818504acc2a3534f22da2fcfa6a18
DomoniqueC <44617815+DomoniqueC@users.noreply.github.com>
2018-10-31 16:56:45 -0500

    Update flightschedule.sql

--- Database System/Tables/Domonique Cox/flightschedule.sql
+++ Database System/Tables/Domonique Cox/flightschedule.sql
@@ -19,4 +19,4 @@ INSERT INTO FlightSchedule VALUES (2144, 'ATL','MIA','10:30 AM','2:30 PM');
 INSERT INTO FlightSchedule VALUES (1441, 'LAX','IAH','7:00 AM', '11:00 AM');
 INSERT INTO FlightSchedule VALUES (7316, 'SLC','ATL', '5:00 PM', '9:00 PM');
 INSERT INTO FlightSchedule VALUES (1114, 'JFK','CLT, '10:20 AM', '3:55 PM');
-INSERT INTO FlightSchedule Values (2136, 'LAS','COS','6:00 AM', '8:50 AM');
+INSERT INTO FlightSchedule VALUES (2136, 'LAS','COS','6:00 AM', '8:50 AM');
