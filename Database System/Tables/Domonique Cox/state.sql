d1704a47e4ba2785146ed1325622e10e4959ae25
DomoniqueC <44617815+DomoniqueC@users.noreply.github.com>
2018-10-31 16:11:38 -0500

    Update state.sql

---
+++ Database System/Tables/Domonique Cox/state.sql
@@ -0,0 +1,9 @@
+CREATE TABLE State( StID INT, StateName Varchar(32), Country Varchar(32), PRIMARY KEY(StID), CONTRAINT fk_Country FOREIGN KEY (Country) REFERENCES Countries (CtId));
+
+
+
+INSERT INTO State VALUES (8, 'Texas','USA');
+INSERT INTO State VALUES (5,'Kansas','USA');
+INSERT INTO State VALUES (3, 'California','USA');
+INSERT INTO State VALUES (1, 'Atlanta', 'USA');
+INSERT INTO State VALUES (7, 'Utah', USA');
