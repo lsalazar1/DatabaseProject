ed3171010eab777a9aa81e7855313e02dee9739a
shawn6293 <35849372+shawn6293@users.noreply.github.com>
2018-11-02 17:38:24 -0500

    Update route.sql

--- Database System/Tables/Shawn Smith/route.sql
+++ Database System/Tables/Shawn Smith/route.sql
@@ -4,3 +4,8 @@ Airport Varchar(32) NOT NULL,
 Destination Varchar(32) NOT NULL,
 RouteCode Varchar(16) NOT NULL UNIQUE,
 PRIMARY KEY (RtID));
+
+INSERT INTO Route VALUES (10,"Dallas-Ft Worth", "Los Angeles", "KDFW-KLAX");
+INSERT INTO Route VALUES (11, "Portland","San Fransico","KPDX-KSFO");
+INSERT INTO Route VALUES (12, "Las Vegas","Denver","KLAS-KDEN");
+INSERT INTO Route VALUES (13, "Atlanta","New York","KATL-KJFK");
