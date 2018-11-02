11780156241c8a9e6c3564d7d4c57ead180a554b
shawn6293 <35849372+shawn6293@users.noreply.github.com>
2018-11-02 17:26:38 -0500

    Update aircraft.sql

--- Database System/Tables/Shawn Smith/aircraft.sql
+++ Database System/Tables/Shawn Smith/aircraft.sql
@@ -4,3 +4,9 @@ CREATE TABLE airCrafts(
     Capacity INT NOT NULL,
     MfdBy Varchar(128) NOT NULL,
     MfdOn Datetime NOT NULL);
+    
+    
+INSERT INTO airCrafts VALUES (100, "N379AA", 245, "Boeing", "March 23, 2004");
+INSERT INTO airCrafts VALUES (101, "N820AL", 372, "Airbus", "June 2, 2011");
+INSERT INTO airCrafts VALUES (102, "N792AN", 382, "Cessna", "April 13, 2010");
+INSERT INTO airCrafts VALUES (103, "N722AN", 298, "Embraer", "October 19, 2008");
