4863e2150d4e3238aa6a59de2967f2e750f341ee
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-02 19:46:23 -0500

    Update discount.sql

--- Database System/Tables/Abrahan Ramirez/discount.sql
+++ Database System/Tables/Abrahan Ramirez/discount.sql
@@ -6,14 +6,14 @@
 
 create table Discounts(
 	DiscountId Int PRIMARY KEY,
-	Title Varchar(32) NOT NULL,
+	Title Varchar(50) NOT NULL,
 	Amount INT NOT NULL,
 	Description Varchar(255)
 );
 
 
-insert into Discounts values(1,'10', 'Discount is provided for children under 10 years old.');
-insert into Discounts values(2,'5', 'Discount is provided for customers with disabilites.');
-insert into Discounts values(3,'65', 'Discount is provided for adutls over the age of 65.');
-insert into Discounts values(4,'30', 'Discount is provided for upgrading to first class.');
+insert into Discounts values(1,'Childrens','10', 'Discount is provided for children under 10 years old.');
+insert into Discounts values(2,'Disabilites Discount', '5', 'Discount is provided for customers with disabilites.');
+insert into Discounts values(3, 'Senior Discount','65', 'Discount is provided for adutls over the age of 65.');
+insert into Discounts values(4,'Deep Pockets Discount', '30','Discount is provided for upgrading to first class.');
 
