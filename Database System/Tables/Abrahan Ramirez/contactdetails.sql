8ec1b52e8b55710f04e1464d2431cbc05d4c470f
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-02 19:36:52 -0500

    Update contactdetails.sql

--- Database System/Tables/Abrahan Ramirez/contactdetails.sql
+++ Database System/Tables/Abrahan Ramirez/contactdetails.sql
@@ -7,7 +7,7 @@
 
 create table Contact_Details(
 	CnID INT PRIMARY KEY,
-	Email VARCHAR(16) NOT NULL,
+	Email VARCHAR(25) NOT NULL,
 	Cell VARCHAR(16) NOT NULL,
 	Street VARCHAR(64),
 	State VARCHAR(20) NOT NULL,
@@ -16,7 +16,7 @@ create table Contact_Details(
 
 
 insert into Contact_Details Values(1, 'jimmyjohn@gmail.com', '949-232-4295', 'Bumpy RD',8);
-insert into Contact_Details Values(2, 'sallymay@gmail.com', '434-234-6746', 'London St.' 5);
+insert into Contact_Details Values(2, 'sallymay@gmail.com', '434-234-6746', 'London St.', 5);
 insert into Contact_Details Values(3, 'SeanPratt@yahoo.com', '765-345-2526', 'Champions Rd',3);
 insert into Contact_Details Values(4, 'Mary3822@gmail.com', '855-354-5474', 'Jurica Rd',1);
 insert into Contact_Details Values(5, 'Danny0493@gmail.com', '1-800-323-9111', 'PV Lane',7);
