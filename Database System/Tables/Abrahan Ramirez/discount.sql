4de3f99e5410ccd178b489b9f4a5703ab92a66f9
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-02 19:14:55 -0500

    Update discount.sql

--- Database System/Tables/Abrahan Ramirez/discount.sql
+++ Database System/Tables/Abrahan Ramirez/discount.sql
@@ -10,3 +10,10 @@ create table Discounts(
 	Amount INT NOT NULL,
 	Description Varchar(255)
 );
+
+
+insert into Discounts values(1,'10', 'Discount is provided for children under 10 years old.');
+insert into Discounts values(2,'5', 'Discount is provided for customers with disabilites.');
+insert into Discounts values(3,'65', 'Discount is provided for adutls over the age of 65.');
+insert into Discounts values(4,'30', 'Discount is provided for upgrading to first class.');
+
