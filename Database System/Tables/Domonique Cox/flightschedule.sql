f34dce6468ba183289b5e36a171587dbaad4c6ca
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-05 12:54:54 -0600

    Update flightschedule.sql

--- Database System/Tables/Domonique Cox/flightschedule.sql
+++ Database System/Tables/Domonique Cox/flightschedule.sql
@@ -14,5 +14,5 @@ ArriveTime Varchar(10) NOT NULL
 INSERT INTO FlightSchedule VALUES (2144, 'ATL','MIA','10:30 AM','2:30 PM');
 INSERT INTO FlightSchedule VALUES (1441, 'LAX','IAH','7:00 AM', '11:00 AM');
 INSERT INTO FlightSchedule VALUES (7316, 'SLC','ATL', '5:00 PM', '9:00 PM');
-INSERT INTO FlightSchedule VALUES (1114, 'JFK','CLT, '10:20 AM', '3:55 PM');
+INSERT INTO FlightSchedule VALUES (1114, 'JFK','CLT', '10:20 AM', '3:55 PM');
 INSERT INTO FlightSchedule VALUES (2136, 'LAS','COS','6:00 AM', '8:50 AM');
