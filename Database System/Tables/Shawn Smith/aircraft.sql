c08bb43111abe64728b8aed41087835f4b309097
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-05 12:34:52 -0600

    Update aircraft.sql

--- Database System/Tables/Shawn Smith/aircraft.sql
+++ Database System/Tables/Shawn Smith/aircraft.sql
@@ -3,10 +3,10 @@ CREATE TABLE airCrafts(
     AcNumber Varchar(32) NOT NULL,
     Capacity INT NOT NULL,
     MfdBy Varchar(128) NOT NULL,
-    MfdOn Datetime NOT NULL);
+    MfdOn Date NOT NULL);
     
-    
-INSERT INTO airCrafts VALUES (100, "N379AA", 245, "Boeing", "March 23, 2004");
-INSERT INTO airCrafts VALUES (101, "N820AL", 372, "Airbus", "June 2, 2011");
-INSERT INTO airCrafts VALUES (102, "N792AN", 382, "Cessna", "April 13, 2010");
-INSERT INTO airCrafts VALUES (103, "N722AN", 298, "Embraer", "October 19, 2008");
+
+INSERT INTO airCrafts VALUES (100, "N379AA", 245, "Boeing", "2004-03-23");
+INSERT INTO airCrafts VALUES (101, "N820AL", 372, "Airbus", "2011-06-02");
+INSERT INTO airCrafts VALUES (102, "N792AN", 382, "Cessna", "2010-04-13");
+INSERT INTO airCrafts VALUES (103, "N722AN", 298, "Embraer", "2008-10-19");
