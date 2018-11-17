d9c6b77d10bf7cdb458d68b3c41f724d0981f6f3
ARamirez14 <44457408+ARamirez14@users.noreply.github.com>
2018-11-16 22:17:03 -0600

    Update contactdetails.sql

--- Database System/Tables/Abrahan Ramirez/contactdetails.sql
+++ Database System/Tables/Abrahan Ramirez/contactdetails.sql
@@ -5,18 +5,17 @@
 **/
 
 
-create table Contact_Details(
-	CnID INT PRIMARY KEY,
+create table ContactDetails(
+	ConID varchar (10) PRIMARY KEY,
 	Email VARCHAR(25) NOT NULL,
 	Cell VARCHAR(16) NOT NULL,
 	Street VARCHAR(64),
-	States VARCHAR(20) NOT NULL,
-	CONSTRAINT fk_State FOREIGN KEY(States) REFERENCES states(StID)
-);
+	states INT  REFERENCES states(StID)
+	);
 
 
-insert into Contact_Details Values(1, 'jimmyjohn@gmail.com', '949-232-4295', 'Bumpy RD',8);
-insert into Contact_Details Values(2, 'sallymay@gmail.com', '434-234-6746', 'London St.', 5);
-insert into Contact_Details Values(3, 'SeanPratt@yahoo.com', '765-345-2526', 'Champions Rd',3);
-insert into Contact_Details Values(4, 'Mary3822@gmail.com', '855-354-5474', 'Jurica Rd',1);
-insert into Contact_Details Values(5, 'Danny0493@gmail.com', '1-800-323-9111', 'PV Lane',7);
+insert into ContactDetails Values(1, 'jimmyjohn@gmail.com', '949-232-4295', 'Bumpy RD',8);
+insert into ContactDetails Values(2, 'sallymay@gmail.com', '434-234-6746', 'London St.', 5);
+insert into ContactDetails Values(3, 'SeanPratt@yahoo.com', '765-345-2526', 'Champions Rd',3);
+insert into ContactDetails Values(4, 'Mary3822@gmail.com', '855-354-5474', 'Jurica Rd',1);
+insert into ContactDetails Values(5, 'Danny0493@gmail.com', '1-800-323-9111', 'PV Lane',7);
