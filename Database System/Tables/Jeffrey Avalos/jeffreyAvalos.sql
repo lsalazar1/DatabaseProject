deadba6e8c79b837d53cbcf6f6b92fcbf753d1ab
JeffreyAvalos <33049132+JeffreyAvalos@users.noreply.github.com>
2018-11-17 22:53:37 -0600

    Update jeffreyAvalos.sql

--- Database System/Tables/Jeffrey Avalos/jeffreyAvalos.sql
+++ Database System/Tables/Jeffrey Avalos/jeffreyAvalos.sql
@@ -24,6 +24,15 @@ create table Passengers(
 
 Insert INTO Passengers VALUES(1,'Jimmy John','Bumpy Rd, Prairie View Tx',45,'USA',1);
 
+INSERT INTO Charges VALUES('5D22B1D5', 'Security', 150.00, 'Security Fee');
+INSERT INTO Charges VALUES('GHIB7253', 'Airline', 500.00, 'Airline ticket');
+INSERT INTO Charges VALUES('4F5D458W', 'Food', 50.00, 'Complementary Food');
+INSERT INTO Charges VALUES('54FDH5JW', 'Tax', 88.00, 'Taxes');
+INSERT INTO Charges VALUES('ZRTG5825', 'Bags', 100.00, 'Luggage Fee');
+INSERT INTO Charges VALUES('54DF78ED', 'Insurance', 90.00, 'Insurance Charge');
+
+INSERT INTO transactions VALUES('129812', '2018-11-24', '2018-12-25', '', '');
+
 
 
 CREATE TABLE Charges(
