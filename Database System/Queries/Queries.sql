3bd24f296473de7bda75b210acf34080f2f61691
Liam Salazar <salazro@win.eng.vzwnet.com>
2018-11-19 00:38:59 -0600

    Edited first business constraint

--- Database System/Queries/Queries.sql
+++ Database System/Queries/Queries.sql
@@ -1,4 +1,15 @@
-Select all the flights going to MIAMI.
+/*Select all the flights going to MIAMI.
 List the prices of the flights leaving before 1pm.
 List all Passengers with more than one transaction.
-List all Passengers that have used a specific discount.
+List all Passengers that have used a specific discount.*/
+
+-- Liam Salazar
+/* Business Question: How many passengers are from each state/province? Show results in desending order. */
+SELECT states.StateName, 
+    COUNT(Contact_Details.States) AS NumberOfPassengers
+FROM Contact_Details
+LEFT JOIN states
+ON Contact_Details.States = states.StID
+GROUP BY StateName
+ORDER By 2 DESC;
+
