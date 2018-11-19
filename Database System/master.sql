97811fc63105414781942188c44ec00b31b439e4
JeffreyAvalos <33049132+JeffreyAvalos@users.noreply.github.com>
2018-11-18 23:48:24 -0600

    Update master.sql
    made discountid from transaction table able to be null

--- Database System/master.sql
+++ Database System/master.sql
@@ -133,8 +133,8 @@ flightNo   INT(4) NOT NULL,
 types     VARCHAR(10) NOT NULL, 
 employee_ID       VARCHAR(4) NOT NULL, 
 charge_ID   VARCHAR(8) NOT NULL, 
-Discount_ID   INT NOT NULL, 
-total     INT NOT NULL,
+Discount_ID   INT, 
+total     DECIMAL(5,2) NOT NULL,
 
 CONSTRAINT fk_TransactionPassenger FOREIGN KEY (passenger_ID)
     REFERENCES Passenger(passenger_ID),
@@ -237,5 +237,17 @@ INSERT INTO Charges VALUES('4F5D458W', 'Food', 50.00, 'Complementary Food');
 INSERT INTO Charges VALUES('54FDH5JW', 'Tax', 88.00, 'Taxes');
 INSERT INTO Charges VALUES('ZRTG5825', 'Bags', 100.00, 'Luggage Fee');
 INSERT INTO Charges VALUES('54DF78ED', 'Insurance', 90.00, 'Insurance Charge');
+
+INSERT INTO transactions VALUES(129812, '2018-11-24', '2018-12-25', 'P01', '2144', 'Cash', 'E1', 'GHIB7253', 1 ,968.00);
+INSERT INTO transactions VALUES(456142, '2018-10-30', '2018-12-25', 'P08', '1441', 'Credit', 'E2', 'GHIB7253', ,978.00);
+INSERT INTO transactions VALUES(619272, '2018-01-24', '2018-11-21', 'P01', '7316', 'Cash', 'E5', 'GHIB7253', 1 ,968.00);
+INSERT INTO transactions VALUES(456784, '2018-03-02', '2018-11-13', 'P09', '2144', 'Cash', 'E6', 'GHIB7253', 3 , 913.00);
+INSERT INTO transactions VALUES(415478, '2018-03-05', '2018-10-30', 'P02', '1114', 'Cash', 'E2', 'GHIB7253', 3, 913.00);
+INSERT INTO transactions VALUES(871871, '2018-10-11', '2018-10-25', 'P03', '2144', 'Credit', 'E4', 'GHIB7253', ,978.00);
+INSERT INTO transactions VALUES(924595, '2018-11-12', '2018-11-13', 'P07', '2136', 'Cash', 'E4', 'GHIB7253', 4, 948.00);
+INSERT INTO transactions VALUES(118285, '2018-11-30', '2018-12-12', 'P04', '7316', 'Check', 'E1', 'GHIB7253', 1, 968.00);
+INSERT INTO transactions VALUES(118652, '2018-09-30', '2018-12-31', 'P05', '1114', 'Credit', 'E2', 'GHIB7253', 2, 973.00);
+INSERT INTO transactions VALUES(118652, '2018-09-30', '2018-12-31', 'P06', '2136', 'Check', 'E1', 'GHIB7253', , 978.00);
+INSERT INTO transactions VALUES(118652, '2018-01-02', '2018-02-01', 'P09', '2136', 'Credit', 'E3', 'GHIB7253', 3, 913.00);
 
 COMMIT;
